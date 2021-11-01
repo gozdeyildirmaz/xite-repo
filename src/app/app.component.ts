f34848bc2b07d6433100fcccb3e05595a4101069
gozdeyildirmaz <gozdeyildirmaz@gmail.com>
2021-11-02 00:54:02 +0300

    -search bar and genres filter are added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {XiteService} from "./xite.service";
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'xite-app';
+  data: any
+  current_data: any
+  searchedText: string = ''
+  genres = new FormControl();
+  genreList: any[] = [];
+  selectedGenres: any[] = [];
+
+  constructor(private xiteservice: XiteService) {
+
+  }
+
+  ngOnInit(): void {
+    this.getDataset()
+  }
+
+  getDataset() {
+    this.xiteservice.getDataset().subscribe(res => {
+
+      this.data = res;
+      this.genreList = res.genres;
+      this.current_data = res.videos;
+    })
+  }
+
+  seached() {
+    this.current_data = this.data.videos.filter((x: any) => {
+        return ((x.title.indexOf && x.title.toLowerCase().indexOf(this.searchedText.toLowerCase()) > -1) || (x.artist.indexOf && x.artist.toLowerCase().indexOf(this.searchedText.toLowerCase()) > -1))
+      }
+    )
+  }
+
+
+  test(event: any) {
+    if (!event) {
+      if (this.selectedGenres.length > 0)
+        this.current_data = this.data.videos.filter((x: any) => {
+            return this.selectedGenres.map(g => g.id).indexOf(x.genre_id) > -1
+          }
+        )
+
+      else {
+        this.current_data = this.data.videos;
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {XiteService} from "./xite.service";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSelectModule} from "@angular/material/select";
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
